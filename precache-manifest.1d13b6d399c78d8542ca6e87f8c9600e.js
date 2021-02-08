0d91d06c89a8164b93df2b8b0402c04839922797
kidchaos04 <vinayakrastogi94@gmail.com>
2021-02-08 15:36:15 +0530

    Updates

---
+++ precache-manifest.1d13b6d399c78d8542ca6e87f8c9600e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d91fb976484ebb3041a6af47b6c78161",
+    "url": "/NewsApp/index.html"
+  },
+  {
+    "revision": "3f7a3b15212468ead463",
+    "url": "/NewsApp/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "cf3a0553961a24f49026",
+    "url": "/NewsApp/static/js/2.a73c47e1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/NewsApp/static/js/2.a73c47e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f7a3b15212468ead463",
+    "url": "/NewsApp/static/js/main.9d16916f.chunk.js"
+  },
+  {
+    "revision": "ed5a952385360db7ab33",
+    "url": "/NewsApp/static/js/runtime-main.66880a59.js"
+  }
+]);
