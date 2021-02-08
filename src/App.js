02f84c1a3ec51b87e0dfcb87ffedd519d3277b10
kidchaos04 <vinayakrastogi94@gmail.com>
2021-02-08 15:34:49 +0530

    deploy my react app to github

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { NewsCards, Modal } from './components';
+import useStyles from './styles';
+
+const themeLight = createMuiTheme({
+  palette: {
+    background: {
+      default: "#f00c1f"
+    },
+    text: {
+      primary: "#fffff7"
+    }
+  }
+});
+
+
+
+
+const App = () => {
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: '3a5ff1c18824821938babb82aa1166ad2e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'instructions') {
+          setIsOpen(true);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <div>
+      <MuiThemeProvider theme={themeLight}>
+      <CssBaseline />
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+          </div>
+        ) : null}
+        <h1>Voice Assisted News Application</h1>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvBiLEsiiDKUmjIr0od_V4rMxiacM2GMMQUw&usqp=CAU" className={classes.alanLogo} alt="logo" />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      {!newsArticles.length ? (
+        <div className={classes.footer}>
+          <Typography variant="body1" component="h2">
+            Created by
+            <a className={classes.link} href="https://www.linkedin.com/in/adrian-hajdin/"> Vinayak Rastogi-- Click the button on the right--></a> 
+          </Typography>
+          
+        </div>
+      ) : null}
+      </MuiThemeProvider>
+    </div>
+  );
+};
+
+export default App;
